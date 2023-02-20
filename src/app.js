1f31eddcf6ae5acabc23098cb7778759600b67d6
begonufer <begonufernandez@gmail.com>
2023-02-20 19:31:54 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -4,8 +4,22 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { arrow } from "@popperjs/core";
+
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon"];
+const extension = [".es", ".com"];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  pronoun.forEach(pronouns => {
+    adj.forEach(adje => {
+      noun.forEach(nouns => {
+        extension.forEach(extensions => {
+          console.log(`${pronouns}${adje}${nouns}${extensions}`);
+        });
+      });
+    });
+  });
 };
